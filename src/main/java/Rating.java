cb7fd24b331624ae7a677034efbc1716a6578183
krasimira-bozhanova <krasimira_bozhanova@abv.bg>
2015-02-20 22:10:39 +0100

    Add very basic structure of classes

---
+++ src/main/java/Rating.java
@@ -0,0 +1,27 @@
+
+public class Rating {
+
+    double ratingValue;
+    Movie movie;
+
+    public Rating(double rating, Movie movie) {
+        this.ratingValue = rating;
+        this.movie = movie;
+    }
+
+    public double getRatingValue() {
+        return ratingValue;
+    }
+
+    public void setRatingValue(double ratingValue) {
+        this.ratingValue = ratingValue;
+    }
+
+    public Movie getMovie() {
+        return this.movie;
+    }
+
+
+
+
+}
