c4d98ec2b86a5372a6306c04acb2f3e5d899795d
Milan Milanov <whizzer.me@gmail.com>
2015-02-21 12:52:49 +0100

    Add w3layouts MovieStore template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
 import static spark.Spark.get;
 
-    public class Main {
-        public static void main(String[] args) {
-            get("/hello", (req, res) -> "Hello World");
-        }
-    }
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import bg.unisofia.fmi.ai.template.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		staticFileLocation("/web");
+
+		get("/hello", (request, response) -> {
+			Map<String, Object> attributes = new HashMap<>();
+			attributes.put("message", "Hello World!");
+
+			return new ModelAndView(attributes, "index.ftl");
+		}, new FreeMarkerEngine());
+
+	}
+}
