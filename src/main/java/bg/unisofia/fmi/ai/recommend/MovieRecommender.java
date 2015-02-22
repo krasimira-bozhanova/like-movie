28731de1015064ea7b9ff84a7196100d1d0c8d46
krasimira-bozhanova <krasimira_bozhanova@abv.bg>
2015-02-22 11:20:15 +0100

    Switch to MovieInfo objects in Main and MovieRecommender

--- src/main/java/bg/unisofia/fmi/ai/recommend/MovieRecommender.java
+++ src/main/java/bg/unisofia/fmi/ai/recommend/MovieRecommender.java
@@ -1,52 +1,68 @@
 package bg.unisofia.fmi.ai.recommend;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import bg.unisofia.fmi.ai.data.Movie;
 import bg.unisofia.fmi.ai.data.User;
+import bg.unisofia.fmi.ai.omdb.MovieInfo;
 
 public class MovieRecommender {
 
     User currentUser;
     KNN currentKNN;
 
-    public List<Movie> getFrontPageMovies(int number) {
-        //TODO - check for logged in user
+    public List<MovieInfo> getFrontPageMovies(int number) {
+        // TODO - check for logged in user
         User loggedInUser = new User("a");
         if (loggedInUser != null) {
             if (!loggedInUser.equals(currentUser)) {
                 currentUser = loggedInUser;
                 currentKNN = new KNN(currentUser, 10);
             }
-            return currentKNN.getLimitedMoviesToRecommend(number);
+            return currentKNN.getLimitedMoviesToRecommend(number).stream()
+                    .map(m -> new MovieInfo(m.getTitle()))
+                    .collect(Collectors.toList());
         }
-        return ColdStart.getRandomMovies(number);
+        return ColdStart.getRandomMovies(number).stream()
+                .map(m -> new MovieInfo(m.getTitle()))
+                .collect(Collectors.toList());
     }
 
-    public List<Movie> getMoviesFromCategory(int number, String category) {
-      //TODO - check for logged in user
+    public List<MovieInfo> getMoviesFromCategory(int number, String category) {
+        // TODO - check for logged in user
         User loggedInUser = new User("a");
         if (loggedInUser != null) {
             if (!loggedInUser.equals(currentUser)) {
                 currentUser = loggedInUser;
                 currentKNN = new KNN(currentUser, 10);
             }
-            return currentKNN.getLimitedMoviesWithCategory(number, category);
+            return currentKNN.getLimitedMoviesWithCategory(number, category).stream()
+                    .map(m -> new MovieInfo(m.getTitle()))
+                    .collect(Collectors.toList());
         }
-        return ColdStart.getRandomMoviesWithCategory(number, category);
+        return ColdStart.getRandomMoviesWithCategory(number, category).stream()
+                .map(m -> new MovieInfo(m.getTitle()))
+                .collect(Collectors.toList());
     }
 
-    public List<Movie> getSimilarMovies(int number, Movie movie) {
-      //TODO - check for logged in user
+    public List<MovieInfo> getSimilarMovies(int number, MovieInfo movieInfo) {
+        Movie movie = Movie.getMovieWithTitle(movieInfo.getTitle());
+        // TODO - check for logged in user
         User loggedInUser = new User("a");
         if (loggedInUser != null) {
             if (!loggedInUser.equals(currentUser)) {
                 currentUser = loggedInUser;
                 currentKNN = new KNN(currentUser, 10);
             }
-            return currentKNN.getLimitedMoviesWithCategory(number, movie.getCategory());
+            return currentKNN.getLimitedMoviesWithCategory(number,
+                    movie.getCategory()).stream()
+                    .map(m -> new MovieInfo(m.getTitle()))
+                    .collect(Collectors.toList());
         }
-        return ColdStart.getSimilarMovies(movie, number);
+        return ColdStart.getSimilarMovies(movie, number).stream()
+                .map(m -> new MovieInfo(m.getTitle()))
+                .collect(Collectors.toList());
     }
 
 }
