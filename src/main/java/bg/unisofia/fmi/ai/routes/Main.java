6c5fa4c6caa8ac6dfdc92069f14e30e5653c49a4
Milan Milanov <whizzer.me@gmail.com>
2015-03-19 22:21:28 +0100

    Implement facebook login

--- src/main/java/bg/unisofia/fmi/ai/routes/Main.java
+++ src/main/java/bg/unisofia/fmi/ai/routes/Main.java
@@ -10,6 +10,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ru.hh.oauth.subscribe.apis.FacebookApi;
+import ru.hh.oauth.subscribe.core.builder.ServiceBuilder;
+import ru.hh.oauth.subscribe.core.model.OAuthRequest;
+import ru.hh.oauth.subscribe.core.model.Response;
+import ru.hh.oauth.subscribe.core.model.Token;
+import ru.hh.oauth.subscribe.core.model.Verb;
+import ru.hh.oauth.subscribe.core.model.Verifier;
+import ru.hh.oauth.subscribe.core.oauth.OAuthService;
 import spark.ModelAndView;
 import bg.unisofia.fmi.ai.dao.GenreService;
 import bg.unisofia.fmi.ai.dao.MovieService;
@@ -27,8 +35,9 @@ import bg.unisofia.fmi.ai.movieinfo.MovieInfoFetcher;
 import bg.unisofia.fmi.ai.template.FreeMarkerEngine;
 import bg.unisofia.fmi.ai.transformers.JsonTransformer;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.j256.ormlite.support.ConnectionSource;
-
 
 public class Main {
     public final static int SIMILAR_MOVIES_NUMBER = 4;
@@ -37,43 +46,72 @@ public class Main {
     private static final String USERNAME_ATTR = "username";
     private static final String USERID_ATTR = "userId";
 
+    private static final Token EMPTY_TOKEN = null;
+    private static final OAuthService service = new ServiceBuilder().provider(FacebookApi.class)
+            .apiKey("1424586531175233").apiSecret("82d013d5d8cf08c6789c2b9f3b1cd50b").scope("user_likes")
+            .callback("http://localhost:4567/oauth_callback_facebook").build();
+    private static final String authUrl = service.getAuthorizationUrl(EMPTY_TOKEN);
+
     public static void main(String[] args) throws IOException, SQLException {
         staticFileLocation("/web");
 
         // DataImporter.movielensImporter("src/main/resources/datasets/movielens/");
         // DataImporter.customWikiExtractedFilesImporter("src/main/resources/datasets/wiki/");
 
-        get("/", (request, response) -> {
-            final ConnectionSource connection = DbUtil.getConnectionSource();
-            final GenreService genreService = new GenreService(connection);
+        get("/",
+                (request, response) -> {
 
-            Map<String, Object> attributes = new HashMap<>();
-            List<MovieInfo> movies = new MovieInfoFetcher(request.session().attribute(USERID_ATTR)).getFrontPageMovies(FRONT_PAGE_MOVIES);
-            attributes.put("genres", genreService.list());
-            attributes.put("selectedGenre", "all");
-            attributes.put("movies", movies);
-            attributes.put("username", request.session().attribute(USERNAME_ATTR));
+                    final ConnectionSource connection = DbUtil.getConnectionSource();
+                    final GenreService genreService = new GenreService(connection);
 
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+                    Map<String, Object> attributes = new HashMap<>();
+                    List<MovieInfo> movies = new MovieInfoFetcher(request.session().attribute(USERID_ATTR))
+                            .getFrontPageMovies(FRONT_PAGE_MOVIES);
+                    attributes.put("genres", genreService.list());
+                    attributes.put("selectedGenre", "all");
+                    attributes.put("movies", movies);
+                    attributes.put("username", request.session().attribute(USERNAME_ATTR));
+                    attributes.put("facebookAuthUrl", authUrl);
 
-        get("/genre/:genreId", (request, response) -> {
-            final ConnectionSource connection = DbUtil.getConnectionSource();
-            final GenreService genreService = new GenreService(connection);
+                    return new ModelAndView(attributes, "index.ftl");
+                }, new FreeMarkerEngine());
 
-            String chosenGenreId = request.params(":genreId");
-            Genre genre = genreService.find(Integer.parseInt(chosenGenreId));
+        get("/oauth_callback_facebook", (request, response) -> {
 
-            Map<String, Object> attributes = new HashMap<>();
-            List<MovieInfo> movies = new MovieInfoFetcher(request.session().attribute(USERID_ATTR)).getMoviesWithGenre(FRONT_PAGE_MOVIES, genre);
-            attributes.put("message", "Hello World!");
-            attributes.put("genres", genreService.list());
-            attributes.put("selectedGenre", genre.getName());
-            attributes.put("movies", movies);
-            attributes.put("username", request.session().attribute(USERNAME_ATTR));
+            Verifier verifier = new Verifier(request.queryParams("code"));
+            Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+            OAuthRequest oauthRequest = new OAuthRequest(Verb.GET, "https://graph.facebook.com/me", service);
+            service.signRequest(accessToken, oauthRequest);
+            Response oauthResponse = oauthRequest.send();
 
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
+            // TODO here decide how to store user id for votes and stuff
+                JsonParser parser = new JsonParser();
+                JsonObject res = (JsonObject) parser.parse(oauthResponse.getBody());
+                request.session().attribute(USERNAME_ATTR, res.get("name").getAsString());
+
+                response.redirect("/");
+                return null;
+            });
+
+        get("/genre/:genreId",
+                (request, response) -> {
+                    final ConnectionSource connection = DbUtil.getConnectionSource();
+                    final GenreService genreService = new GenreService(connection);
+
+                    String chosenGenreId = request.params(":genreId");
+                    Genre genre = genreService.find(Integer.parseInt(chosenGenreId));
+
+                    Map<String, Object> attributes = new HashMap<>();
+                    List<MovieInfo> movies = new MovieInfoFetcher(request.session().attribute(USERID_ATTR))
+                            .getMoviesWithGenre(FRONT_PAGE_MOVIES, genre);
+                    attributes.put("message", "Hello World!");
+                    attributes.put("genres", genreService.list());
+                    attributes.put("selectedGenre", genre.getName());
+                    attributes.put("movies", movies);
+                    attributes.put("username", request.session().attribute(USERNAME_ATTR));
+
+                    return new ModelAndView(attributes, "index.ftl");
+                }, new FreeMarkerEngine());
 
         get("/register", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
@@ -112,6 +150,7 @@ public class Main {
         }, new FreeMarkerEngine());
 
         post("/login", (request, response) -> {
+
             final ConnectionSource connection = DbUtil.getConnectionSource();
             final UserService userService = new UserService(connection);
 
@@ -140,28 +179,30 @@ public class Main {
             return request;
         });
 
-        get("/movies/:movieId", (request, response) -> {
-            final ConnectionSource connection = DbUtil.getConnectionSource();
-            final GenreService genreService = new GenreService(connection);
-            final RatingService ratingService = new RatingService(connection);
+        get("/movies/:movieId",
+                (request, response) -> {
+                    final ConnectionSource connection = DbUtil.getConnectionSource();
+                    final GenreService genreService = new GenreService(connection);
+                    final RatingService ratingService = new RatingService(connection);
 
-            final WatchingService watchingService = new WatchingService(connection);
+                    final WatchingService watchingService = new WatchingService(connection);
 
-            //int userId = request.session().attribute(USERID_ATTR);
-            final MovieInfoFetcher fetcher = new MovieInfoFetcher(request.session().attribute(USERID_ATTR));
+                    // int userId = request.session().attribute(USERID_ATTR);
+                    final MovieInfoFetcher fetcher = new MovieInfoFetcher(request.session().attribute(USERID_ATTR));
 
-            int chosenMovieId = Integer.parseInt(request.params(":movieId"));
-            MovieInfo movieInfo = fetcher.getMovie(chosenMovieId);
+                    int chosenMovieId = Integer.parseInt(request.params(":movieId"));
+                    MovieInfo movieInfo = fetcher.getMovie(chosenMovieId);
 
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("genres", genreService.list());
-            attributes.put("movie", movieInfo);
-            attributes.put("username", request.session().attribute(USERNAME_ATTR));
-            attributes.put("movies", fetcher.getSimilarMovies(SIMILAR_MOVIES_NUMBER, chosenMovieId));
-            attributes.put("liked", ratingService.find(request.session().attribute(USERID_ATTR), chosenMovieId));
-            attributes.put("watched", watchingService.find(request.session().attribute(USERID_ATTR), chosenMovieId));
-                return new ModelAndView(attributes, "preview.ftl");
-            }, new FreeMarkerEngine());
+                    Map<String, Object> attributes = new HashMap<>();
+                    attributes.put("genres", genreService.list());
+                    attributes.put("movie", movieInfo);
+                    attributes.put("username", request.session().attribute(USERNAME_ATTR));
+                    attributes.put("movies", fetcher.getSimilarMovies(SIMILAR_MOVIES_NUMBER, chosenMovieId));
+                    attributes.put("liked", ratingService.find(request.session().attribute(USERID_ATTR), chosenMovieId));
+                    attributes.put("watched",
+                            watchingService.find(request.session().attribute(USERID_ATTR), chosenMovieId));
+                    return new ModelAndView(attributes, "preview.ftl");
+                }, new FreeMarkerEngine());
 
         get("/movies", (request, response) -> {
             final ConnectionSource connection = DbUtil.getConnectionSource();
@@ -181,12 +222,12 @@ public class Main {
             Movie movie = movieService.find(chosenMovieId);
             User user = userService.find(request.session().attribute(USERID_ATTR));
             // TODO: think about the value of rating
-            Rating newRating = new Rating(user, movie, 1);
-            ratingService.save(newRating);
-            response.redirect("/movies/" + chosenMovieId);
+                Rating newRating = new Rating(user, movie, 1);
+                ratingService.save(newRating);
+                response.redirect("/movies/" + chosenMovieId);
 
-            return null;
-        });
+                return null;
+            });
 
         get("/movies/:movieId/watch", (request, response) -> {
             final ConnectionSource connection = DbUtil.getConnectionSource();
@@ -221,7 +262,6 @@ public class Main {
             response.redirect("/movies/" + chosenMovieId);
             return null;
         });
-
 
     }
 }
